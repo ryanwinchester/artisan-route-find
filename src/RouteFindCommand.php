04da0f61a989c039e436846abef8d7444d78dba6
Ryan Winchester <hello@ryanwinchester.ca>
2016-10-07 21:56:15 -0700

    Highlight results

--- src/RouteFindCommand.php
+++ src/RouteFindCommand.php
@@ -29,35 +29,37 @@ class RouteFindCommand extends Command
      * @return int
      */
      public function handle()
-     {
-         $process = new Process("php artisan route:list");
-         $process->run();
+    {
+        $process = new Process("php artisan route:list");
+        $process->run();
 
-         $results = $this->searchResults(
-             explode("\n", $process->getOutput())
-         );
+        $results = $this->searchResults(
+            $this->argument('search'),
+            explode("\n", $process->getOutput())
+        );
 
-         if (! empty($results)) {
-           print $results;
-         } else {
-           $this->info("No results.");
-         }
+        if (! empty($results)) {
+          print $results;
+        } else {
+          $this->info("No results.");
+        }
 
-         return $process->getExitCode();
-     }
+        return $process->getExitCode();
+    }
 
     /**
      * Format the output into a string.
      *
+     * @param  string  $search
      * @param  array  $output
      * @return string
      */
-    private function searchResults($output)
+    private function searchResults($search, $output)
     {
-        return collect($output)->filter(function ($line) {
-            return str_contains($line, $this->argument('search'));
-        })->map(function ($line) {
-            return $this->formatLine($line);
+        return collect($output)->filter(function ($line) use ($search) {
+            return str_contains($line, $search);
+        })->map(function ($line) use ($search) {
+            return $this->formatLine($line, $search);
         })->implode(PHP_EOL);
     }
 
@@ -65,23 +67,31 @@ class RouteFindCommand extends Command
      * Format a search result line.
      *
      * @param  string  $line
+     * @param  string  $search
      * @return mixed
      */
-    private function formatLine($line)
-        {
-            if ($this->option('trim')) {
-                $line = preg_replace('/\s\s+/', ' ', $line);
-            }
+    private function formatLine($line, $search)
+    {
+        if ($this->option('trim')) {
+            $line = preg_replace('/\s\s+/', ' ', $line);
+        }
 
-            // TODO: Add search term highlighting
-            // e.g. pseudo-ish code:
-            //
-            // return str_replace(
-            //     $this->argument('search'),
-            //     "highlight".$search."/highlight",
-            //     $line
-            // );
+        return $this->highlight($search, $line);
+    }
 
-            return $line;
+    /**
+     * Highlight a string inside of a string.
+     *
+     * @param  string  $needle
+     * @param  string  $haystack
+     * @return mixed
+     */
+    private function highlight($needle, $haystack)
+    {
+        return str_replace(
+            $needle,
+            "\e[93m{$needle}\e[39m",
+            $haystack
+        );
     }
 }
